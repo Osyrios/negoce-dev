05b55476c07d75737b57ceca4326e19c07880767
yassine <yassine.n13@gmail.com>
2024-03-13 17:52:32 +0100

    fixe contact link

--- src/Components/GlobalSections/Footer.jsx
+++ src/Components/GlobalSections/Footer.jsx
@@ -6,26 +6,34 @@ import githubWhite from '../../assets/social/whiteGithub.png'
 
 export function Footer({anchor}){
 
+    const mail = "mailto:yassine.n13@gmail.com"
+    const tel = "tel:+33649636791"
+
+
     const socials = [
         {
             "name":"yassine.n13@gmail.com",
             "logo": email,
-            "link":""
+            "link":{mail},
+            "blank": ""
         },
         {
             "name":"06.49.63.67.91",
             "logo": phone,
-            "link":""
+            "link":{tel},
+            "blank": ""
         },
         {
             "name":"Linkedin",
             "logo": linkedin,
-            "link":"https://fr.linkedin.com/in/yassine-n%C3%A9goce-78b14512b"
+            "link":"https://fr.linkedin.com/in/yassine-n%C3%A9goce-78b14512b",
+            "blank": "_blank"
         },
         {
             "name":"Github",
             "logo": githubWhite,
-            "link":"https://github.com/Osyrios"
+            "link":"https://github.com/Osyrios",
+            "blank": "_blank"
         }
     ]
 
@@ -36,7 +44,7 @@ export function Footer({anchor}){
         <div className="flex flex-col md:flex-row md:w-auto justify-center items-center m-auto pb-8 pt-2 ">
             {socials.map((social) =>
                 <div key={social.name} className="md:w-2/6 flex flex-row flex-start md:flex-col items-center">
-                    <a href={social.link ? social.link : "#"} target={social.link ? "_blank" : ""} className="w-2/6 flex justify-end md:justify-center m-2 md:w-auto md:m-0">
+                    <a href={social.link ? social.link : "#"} target={social.blank} className="w-2/6 flex justify-end md:justify-center m-2 md:w-auto md:m-0">
                         <img src={social.logo} alt={social.name} className="w-1/6"/>
                     </a>
                     <p className="block">{social.name}</p>
