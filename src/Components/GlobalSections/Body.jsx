d2ed7f7af3d7ada8d18a9c00700eaeebb870b6bb
Yassine <yassine.n13@gmail.com>
2024-03-13 10:26:53 +0100

    Fixing anchors displayed from the navbar

--- src/Components/GlobalSections/Body.jsx
+++ src/Components/GlobalSections/Body.jsx
@@ -12,13 +12,13 @@ export function Body(){
     return <div className="h-auto bg-gray-400">
             <div className="mx-2 p-2 md:mx-32 md:p-4 xl:mx-52 xl:p-8">
                 <ArrowToTheTop/>
-                <MainTitle title={"Développeur d'applications"} anchor={'aboutMe'}/>
-                <SectionTitle title={"Qui suis-je ?"}/>
+                <MainTitle title={"Développeur d'applications"}/>
+                <SectionTitle title={"Qui suis-je ?"} anchor={'aboutMe'}/>
                 <AboutMe/>
                 <SectionTitle title={"Mes compétences"}/>
                 <SectionTitle title={"Mes projets"} anchor={'projects'}/>
                 <Projects/>
-                <SectionTitle title={"Mon Parcours"} anchor={'path'}/>
+                <SectionTitle title={"Mon Parcours"} anchor={'path'} />
                 <ProfessionalPath/>
                 <SectionTitle title={"Formations"}/>
                 <SchoolPath/>
--- src/Components/Titles/MainTitle.jsx
+++ src/Components/Titles/MainTitle.jsx
@@ -1,5 +1,5 @@
 export function MainTitle({title, anchor}) {
-    return <div className="text-center font-designer py-5 md:text-start lg:mb-16 lg:mt-8 flex flex-row justify-center text-transparent bg-clip-text bg-gradient-to-b from-blue-900 to-gray-500"
+    return <div className="text-center font-designer pt-5 pb-2 md:text-start lg:mb-8 lg:mt-8 flex flex-row justify-center text-transparent bg-clip-text bg-gradient-to-b from-blue-900 to-gray-500"
                 id={anchor}>
         <h1 className="font-bold lg:text-3xl text-2xl"> {title} </h1>
     </div>
