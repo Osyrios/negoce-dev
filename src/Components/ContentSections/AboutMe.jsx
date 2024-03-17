3846fa1d2b67505641c881fbec2b57866506e1f0
Yassine Négoce <132480881+Osyrios@users.noreply.github.com>
2024-03-17 16:18:37 +0100

    Update AboutMe.jsx

--- src/Components/ContentSections/AboutMe.jsx
+++ src/Components/ContentSections/AboutMe.jsx
@@ -30,7 +30,7 @@ export function AboutMe(){
                         </p>
 
                         <p className="pb-3">
-                            Me voilà donc titulaire d&apos;un et en pleine recherche d&apos;opportunités !<br/>
+                            Me voilà donc titulaire d&apos;un bachelor et en pleine recherche d&apos;opportunités !<br/>
                             En attendant, je continue de m&apos;exercer et j&apos;ajouterais au fur et à mesure mes projets à ce site !
                         </p>
 
