59b890ed0b6860bae5a7d5dc50a80b4236f31d92
yassine <yassine.n13@gmail.com>
2024-05-27 07:33:27 +0200

    change text

--- src/Components/ContentSections/AboutMe.jsx
+++ src/Components/ContentSections/AboutMe.jsx
@@ -21,37 +21,43 @@ export function AboutMe(){
                      className="appearFromRight ellip rounded-full mt-4 h-1/3 w-1/3 text-center md:h-2/6 md:w-2/6 lg:h-2/5 lg:w-2/5 xl:h-1/5 xl:w-1/5 xl:ms-10"
                 />
             <div className="appearFromBottom flex flex-col items-center xl:w-1/2">
-                    <h2 className=" font-robotoBold mb-3 text-lg"> Bienvenue sur mon portfolio en ligne !</h2>
-                    <div className="py-2 text-justify text-sm">
+                <h2 className=" font-robotoBold mb-3 text-lg"> Bienvenue !</h2>
+                <div className="py-2 text-justify text-sm">
 
-                        <p className="pb-3">
-                            Je m&apos;appelle Yassine j&apos;ai 28 ans et je suis passionné par le développement d&apos;applications !
-                        </p>
+                    <p className="pb-3">
+                        Je m&apos;appelle Yassine j&apos;ai 28 ans et je suis passionné par le développement
+                        d&apos;applications !
+                    </p>
 
-                        <p className="pb-3">
-                            Après 8 ans en tant que comptable, il est temps pour moi de quitter ce milieu qui ne me correspond pas.
-                        </p>
+                    <p className="pb-3">
+                        Après 8 ans en tant que comptable, il est temps pour moi de quitter ce milieu pour le monde du
+                        développement.
+                    </p>
 
-                        <p className="pb-3">
-                            Mi-2022, j&apos;ai entamé mon aventure dans la reconversion professionnelle afin de pouvoir enfin lier travaille et passion !
-                        </p>
+                    <p className="pb-3">
+                        Mi-2022, j&apos;ai entamé mon aventure dans la reconversion professionnelle afin de pouvoir
+                        enfin lier travail et passion !
+                    </p>
 
-                        <p className="pb-3">
-                            C&apos;est donc après une remise a la pratique du code que j&apos;ai débuté ma formation de développeur chez Studi !<br/>
-                            Une formation suivie en parallèle de mon métier et de ma vie de famille et de laquelle je suis sorti diplômé d'un
-                            bachelor &quot;Développeur de solutions digitales&quot; !
-                        </p>
+                    <p className="pb-3">
+                        C&apos;est donc après une remise a la pratique du code que j&apos;ai débuté ma formation de
+                        développeur chez Studi !<br/>
+                        Une formation suivie en parallèle de mon métier et de ma vie de famille et de laquelle je suis
+                        sorti diplômé d&apos;un
+                        bachelor &quot;Développeur de solutions digitales&quot; !
+                    </p>
 
-                        <p className="pb-3">
-                            Me voilà donc titulaire d&apos;un bachelor et en pleine recherche d&apos;opportunités !<br/>
-                            En attendant, je continue de m&apos;exercer et j&apos;ajouterais au fur et à mesure mes projets à ce site !
-                        </p>
+                    <p className="pb-3">
+                        Me voilà donc titulaire d&apos;un bachelor et en pleine recherche d&apos;opportunités !<br/>
+                        En attendant, je continue de m&apos;exercer et j&apos;ajouterais au fur et à mesure mes projets
+                        à ce site !
+                    </p>
 
-                        <p className="pt-2 text-center font-robotoItalic text-lg ">
-                            Bonne visite !
-                        </p>
+                    <p className="pt-2 text-center font-robotoItalic text-lg ">
+                        Bonne visite !
+                    </p>
 
-                    </div>
+                </div>
             </div>
         </div>
 
